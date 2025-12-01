ec2f2606bdc47f6274356b0988d7ad3300192f04
tinkin <tinkin@tinkin.home>
2025-12-01 15:53:30 +0000

    Ajout du dossier de test pour postman

--- src/main/java/com/odc/om/paie/authenticated/config/DataInitializer.java
+++ src/main/java/com/odc/om/paie/authenticated/config/DataInitializer.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 
 @Component
 @RequiredArgsConstructor
